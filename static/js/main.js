e2659c1287ee2a68ebfd9534e5a02c95225ac75e
ponti <71143849+pontiggia@users.noreply.github.com>
2021-08-04 01:19:57 -0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-const btnDelete= document.querySelectorAll('.btn-delete');
+const btnDelete = document.querySelectorAll('.btn-delete');
 if(btnDelete) {
   const btnArray = Array.from(btnDelete);
   btnArray.forEach((btn) => {
