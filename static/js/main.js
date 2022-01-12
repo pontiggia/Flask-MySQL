937d961a27dc14bcae49bdee741136bd4645a379
ponti <71143849+pontiggia@users.noreply.github.com>
2022-01-12 04:52:51 -0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
-const btnDelete = document.querySelectorAll('.btn-delete');
+const btnDelete = document.querySelectorAll('.btn-delete'); 
 if(btnDelete) {
-  const btnArray = Array.from(btnDelete);
+  const btnArray = Array.from(btnDelete); // function that pop up a notification when you are trying to delete a contact
   btnArray.forEach((btn) => {
     btn.addEventListener('click', (e) => {
       if(!confirm('¿Estas seguro que quieres eliminar este contacto?')){
